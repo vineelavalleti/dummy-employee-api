3dc6a2a78432714aa8fb2defd5f2c85391cb4b49
vineelavalleti <vineela.valleti@gmail.com>
2023-07-12 17:52:56 -0400

    Initial dummy project!

---
+++ src/main/java/com/vineela/dummy/emp/api/DummyEmployeeApiApplication.java
@@ -0,0 +1,13 @@
+package com.vineela.dummy.emp.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DummyEmployeeApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DummyEmployeeApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vineela/dummy/emp/api/DummyEmployeeApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vineela.dummy.emp.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DummyEmployeeApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
