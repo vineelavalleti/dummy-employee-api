2367034796dec24e7b791eef6c1d250d48712ab9
vineelavalleti <vineela.valleti@gmail.com>
2023-07-12 19:09:10 -0400

    Code Review comments fixed

--- src/main/java/com/vineela/dummy/emp/api/controller/HelloController.java
+++ src/main/java/com/vineela/dummy/emp/api/controller/HelloController.java
@@ -1,27 +1,30 @@
 package com.vineela.dummy.emp.api.controller;
 
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.vineela.dummy.emp.api.constants.EmpConstants;
+
 @RestController
-@RequestMapping("/api/v1")
+@RequestMapping(EmpConstants.EMP_API_ROOT_PATH)
 public class HelloController {
 	
-	@RequestMapping(value = "/hello", method=RequestMethod.GET)
-	@GetMapping("/hello")
+
+	@RequestMapping(value = EmpConstants.EMP_API_HELLO_PATH, 
+			method=RequestMethod.GET)
+	
 	public String getHello(){
 		return "Good Morning";
 	}
-	@RequestMapping(value = "/hi",
+	@RequestMapping(value = EmpConstants.EMP_API_HI_PATH,
 		    method = RequestMethod.POST,
 		    produces = "application/json",
 		    consumes = MediaType.APPLICATION_JSON_VALUE
 		)
 	public String getHi(){
+		
 		return " {\"message\":\"Good Morning\"} ";
 	}
 }
